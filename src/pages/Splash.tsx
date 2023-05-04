8ffe601bb359868f3aefbe164c4f515490dfd454
Rilson Oliveira <rilson2.0@gmail.com>
2023-05-04 15:42:03 -0300

    feat: Add loadUserData function on Splash page.

--- src/pages/Splash.tsx
+++ src/pages/Splash.tsx
@@ -1,19 +1,40 @@
 import { useNavigation } from '@react-navigation/native';
-import { Center } from 'native-base';
+import { Center, useToast } from 'native-base';
 import { useEffect } from 'react';
 import * as Animatable from 'react-native-animatable';
 import { NavigatorRoutesProps } from 'src/routes';
 import LogoImage from '@assets/codeleap_logo_black.png';
+import { storageUserGet } from '@storage/storageUser';
+import { AppError } from '@utils/AppError';
 
 export function Splash() {
+  const toast = useToast();
   const { reset } = useNavigation<NavigatorRoutesProps>();
+
+  async function loadUserData() {
+    try {
+      const user = await storageUserGet();
+      reset({
+        index: 0,
+        routes: [{ name: user?.name ? 'home' : 'signIn' }],
+      });
+    } catch (error) {
+      const isAppError = error instanceof AppError;
+      const title = isAppError
+        ? error.message
+        : 'Failed to load user data. Try again later.';
+
+      toast.show({
+        title,
+        placement: 'top',
+        bgColor: 'red.500',
+      });
+    }
+  }
 
   useEffect(() => {
     setTimeout(() => {
-      reset({
-        index: 0,
-        routes: [{ name: 'signIn' }],
-      });
+      loadUserData();
     }, 2000);
   });
 
